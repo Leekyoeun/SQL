e7e27807ecbc21d3e525463a5be667990e3db422
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2022-02-02 19:16:27 +0900

    SQL 문제해설 #2
    SQL 문제해설 #2

--- practice.sql
+++ practice.sql
@@ -73,17 +73,48 @@ SELECT employee_id, last_name || first_name, job_id, salary, hire_date, departme
 FROM employees
 WHERE salary>=1100 AND job_id='ST_MAN';
 
---20.EMPLOYEES 테이블에서 급여가 10000이상이거나 JOB이 Manager 인 사원의 정보를 사원번호, 성명, 담당업무, 급여, 입사일자, 부서번호를 출력하라.
+--20. EMPLOYEES 테이블에서 급여가 10000이상이거나 JOB이 Manager 인 사원의 정보를 사원번호, 성명, 담당업무, 급여, 입사일자, 부서번호를 출력하라.
 SELECT employee_id, last_name || first_name, job_id, salary, hire_date, department_id 
 FROM employees
 WHERE salary>=1000 OR job_id='ST_MAN';
 
---21.EMPLOYEES 테이블에서 JOB이 ST_MAN, SA_MAN, SA_REP가 아닌 사원의 정보를 사원번호, 성명, 담당업무, 급여, 부서번호를 출력하라.
+--21. EMPLOYEES 테이블에서 JOB이 ST_MAN, SA_MAN, SA_REP가 아닌 사원의 정보를 사원번호, 성명, 담당업무, 급여, 부서번호를 출력하라.
 SELECT employee_id, last_name || first_name, job_id, salary, department_id 
 FROM employees
 WHERE job_id NOT in('ST_MAN','SA_MAN','SA_REF');
 
---22.업무가 PRESIDENT(AD_PRES) 이고 급여가 12000이상이거나 업무가 SA_MAN인 사원의 정보를 사원번호, 이름, 업무, 급여를 출력하라.
+--22. 업무가 PRESIDENT(AD_PRES) 이고 급여가 12000이상이거나 업무가 SA_MAN인 사원의 정보를 사원번호, 이름, 업무, 급여를 출력하라.
 SELECT employee_id, last_name || first_name, job_id, salary 
 FROM employees
 WHERE job_id='AD_PRES' AND salary>=1200 OR job_id='SA_MAN';
+
+--23. 업무가 PRESIDENT(AD_PRES) 또는 SA_MAN 이고 급여가 12000이상이거나 사원의 정보를 사원번호, 이름, 업무, 급여를 출력하라. 
+SELECT employee_id, last_name || first_name, job_id, salary 
+FROM employees
+WHERE job_id='AD_PRES' OR job_id='SA_MAN' AND salary>=1200;
+
+--24. EMPLOYEES 테이블에서 입사일자 순으로 정렬하여 사원번호, 이름, 업무, 급여, 입사일자, 부서번호를 출력하라. 
+SELECT employee_id, last_name || first_name, job_id, salary, hire_date, department_id 
+FROM employees 
+ORDER BY hire_date;
+
+--25. EMPLOYEES 테이블에서 가장 최근에 입사한 순으로 사원번호, 이름, 업무, 급여, 입사일자, 부서번호를 출력하라.
+SELECT last_name, job_id, salary, hire_date, department_id 
+FROM employees 
+ORDER BY hire_date DESC;
+
+--26. EMPLOYEES 테이블에서 부서번호로 정렬한 후 부서번호가 같을 경우 급여가 많은 순으로 정렬하여 사원번호, 성명, 업무, 부서번호, 급여를 출력하여라.
+SELECT employee_id, last_name || first_name, job_id, department_id, salary 
+FROM employees 
+ORDER BY department_id,salary desc;
+
+--27. EMPLOYEES 테이블에서 첫번째 정렬은 부서번호로 두번째 정렬은 업무로 세번째정렬은 급여가 많은 순으로 정렬하여 사원번호, 성명, 입사일자, 부서번호, 업무, 급여를 출력하여라.
+SELECT employee_id, last_name || first_name, hire_date, department_id, job_id, salary 
+FROM employees 
+ORDER BY department_id, job_id, salary desc;
+
+--28. EMPLOYEES 테이블에서 King의 정보를 소문자로 검색하고 사원번호, 성명, 담당업무(소문자로), 부서번호를 출력하라.
+SELECT employee_id, last_name || first_name, job_id, department_id 
+FROM employees 
+WHERE lower(last_name) like lower('king');
+
--- scott.sql
+++ scott.sql
@@ -13,3 +13,46 @@ SELECT distinct job FROM emp;
 --5. emp 테이블의 사원명과 업무로 연결(SMITH, CHECK)해서 표시하고 컬럼명은 Employee and job으로 표시하시오.
 SELECT ename || 'smith' || job || 'check' "employee and job" FROM emp;
 
+--6.emp 테이블에서 사원번호가 7698인 사원의 이름, 업무, 급여를 출력하시오.
+SELECT ename, job, sal, mgr FROM emp WHERE mgr='7698';
+
+--7. emp 테이블에서 사원이름이 SMITH인 사람의 이름과 월급, 부서번호를 구하시오.
+SELECT ename, sal, deptno FROM emp WHERE ename='SMITH';
+
+--8. 월급이 2500이상 3500미만인 사원의 이름, 입사일, 월급을 구하시오.
+SELECT ename, hiredate, sal FROM emp WHERE sal>=2500 and sal<3500;
+
+--9. 급여가 2000에서 3000사이에 포함되지 않는 사원의 이름, 업무, 급여를 출력하시오.
+SELECT ename, job, sal FROM emp WHERE sal not between 2000 and 3000;
+
+--10. 81년05월01일과 81년12월03일 사이에 입사한 사원의 이름, 급여, 입사일을 출력하시오. 
+SELECT ename, sal, hiredate 
+FROM emp 
+WHERE hiredate between '81/05/01' and '81/12/03';
+
+--11. emp테이블에서 사원번호가 7566,7782,7934인 사원을 제외한 사람들의 사원번호,이름, 월급을 출력하시오.
+SELECT empno, ename, sal FROM EMP WHERE empno not in('7566','7782','7934');
+
+--12. 부서번호 30(deptno)에서 근무하며 월2,000달러 이하를 받는 81년05월01일 이전에 입사한 사원의 이름, 급여, 부서번호, 입사일을 출력하시오.
+SELECT ename, sal, deptno, hiredate 
+FROM emp 
+WHERE deptno=30 and sal<2000 and hiredate <='81.05.01';
+
+--13. emp테이블에서 급여가 2,000와 5,000 사이고 부서번호가 10 또는 30인 사원의 이름과 급여,부서번호를 나열하시오. 
+SELECT ename, sal, deptno 
+FROM emp 
+WHERE sal between 2000 and 5000 and deptno=10 or deptno=30;
+
+--14. 업무가 SALESMAN 또는 MANAGER이면서 급여가 1,600, 2,975 또는 2,850이 아닌 모든 사원의 이름, 업무 및 급여를 표시하시오.
+SELECT ename, job, sal 
+FROM emp 
+WHERE job='SALESMAN' OR job='MANAGER' AND sal not in('1600','2975','2850');
+
+--15. emp테이블에서 이름에 A와 E가 있는 모든 사원의 이름을 표시하시오.
+SELECT ename FROM emp WHERE ename like '%A%' or ename like '%E%';
+
+--16. emp테이블에서 관리자가 없는 모든 사원의 이름과 업무를 표시하시오.
+SELECT ename, job FROM emp WHERE job !='MANAGER';
+
+--17. emp테이블에서 커미션 항목이 입력된 사원들의 이름과 급여, 커미션을 구하시오.
+SELECT ename, sal, comm FROM emp WHERE comm IS NOT NULL;
